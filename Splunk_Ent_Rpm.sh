2c827a14dfb53cd4766b29a0b81cd36f17d1a2ce
CV4no <91588197+CV4no@users.noreply.github.com>
2021-09-29 11:28:19 -0400

    Update Splunk_Ent_Rpm.sh

--- Splunk_Ent_Rpm.sh
+++ Splunk_Ent_Rpm.sh
@@ -1,10 +1,8 @@
 wget -O splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm 'https://d7wz6hmoaavd0.cloudfront.net/products/splunk/releases/8.2.2.1/linux/splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm';
 rpm -i splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm;
-cd /opt/splunk/bin;
 ./splunk start --accept-license;
 ./splunk stop;
 ./splunk enable boot-start -user splunk;
 chown -R splunk:splunk /opt/splunk;
 su - splunk;
-cd /opt/splunk/bin;
 ./splunk start;
