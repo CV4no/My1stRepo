066ad42430722a194bc99a804a682f8a0aae4487
CV4no <91588197+CV4no@users.noreply.github.com>
2021-09-29 12:08:02 -0400

    Create Splunk_UF_Deb.sh

---
+++ Splunk_UF_Deb.sh
@@ -0,0 +1,10 @@
+wget -O splunkforwarder-8.2.0-e053ef3c985f-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.2.0&product=universalforwarder&filename=splunkforwarder-8.2.0-e053ef3c985f-linux-2.6-amd64.deb&wget=true';
+dpkg -i splunkforwarder-8.2.0-e053ef3c985f-linux-2.6-amd64.deb;
+cd /opt/splunkforwarder/bin;
+./splunk start --accept-license;
+./splunk stop;
+./splunk enable boot-start -user splunk;
+chown -R splunk:splunk /opt/splunkforwarder;
+su - splunk;
+cd /opt/splunkforwarder/bin;
+./splunk start;
