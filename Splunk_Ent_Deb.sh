d55b9ec0b1b8469216a6a8b0fa97cbae49fadca8
CV4no <91588197+CV4no@users.noreply.github.com>
2021-09-29 02:48:13 -0400

    Update Splunk_Ent_Deb.sh

--- Splunk_Ent_Deb.sh
+++ Splunk_Ent_Deb.sh
@@ -1,5 +1,5 @@
 wget -O splunk-8.2.0-e053ef3c985f-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.2.0&product=splunk&filename=splunk-8.2.0-e053ef3c985f-linux-2.6-amd64.deb&wget=true';
-dpkg -i splunk-8.2.0-e053ef3c985f-linux-2.6-amd64.deb /opt;
+dpkg -i splunk-8.2.0-e053ef3c985f-linux-2.6-amd64.deb;
 cd /opt/splunk/bin;
 ./splunk start --accept-license;
 ./splunk stop;
