26750816e6336533d44dedd5037e32f83cfbb3c1
CV4no <91588197+CV4no@users.noreply.github.com>
2021-10-28 09:59:54 -0400

    Update Splunk_Ent_Deb.sh

--- Splunk_Ent_Deb.sh
+++ Splunk_Ent_Deb.sh
@@ -1,10 +1,10 @@
-wget -O splunk-8.2.0-e053ef3c985f-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.2.0&product=splunk&filename=splunk-8.2.0-e053ef3c985f-linux-2.6-amd64.deb&wget=true';
-dpkg -i splunk-8.2.0-e053ef3c985f-linux-2.6-amd64.deb;
-cd /opt/splunk/bin;
-./splunk start --accept-license;
-./splunk stop;
-./splunk enable boot-start -user splunk;
-chown -R splunk:splunk /opt/splunk;
+sudo wget -O splunk-8.2.0-e053ef3c985f-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.2.0&product=splunk&filename=splunk-8.2.0-e053ef3c985f-linux-2.6-amd64.deb&wget=true';
+sudo dpkg -i splunk-8.2.0-e053ef3c985f-linux-2.6-amd64.deb;
+sudo cd /opt/splunk/bin;
+sud0 ./splunk start --accept-license;
+sudo ./splunk stop;
+sudo ./splunk enable boot-start -user splunk;
+sudo chown -R splunk:splunk /opt/splunk;
 su - splunk;
 cd /opt/splunk/bin;
-/opt/splunkforwarder/bin/splunk start;
+sudo /opt/splunkforwarder/bin/splunk start;
