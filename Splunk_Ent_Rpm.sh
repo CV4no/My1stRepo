8c04d7ae52db06c275f401d6cf1044d3bba2c311
CV4no <91588197+CV4no@users.noreply.github.com>
2021-10-28 10:14:18 -0400

    Update Splunk_Ent_Rpm.sh

--- Splunk_Ent_Rpm.sh
+++ Splunk_Ent_Rpm.sh
@@ -1,12 +1,11 @@
-wget -O splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm 'https://d7wz6hmoaavd0.cloudfront.net/products/splunk/releases/8.2.2.1/linux/splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm';
-rpm -i splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm;
-cd /opt/splunk/bin;
-./splunk start --accept-license;
-./splunk stop;
-./splunk enable boot-start -user splunk;
-echo "$USER";
-chown -R splunk:splunk /opt/splunk;
+sudo wget -O splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm 'https://d7wz6hmoaavd0.cloudfront.net/products/splunk/releases/8.2.2.1/linux/splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm';
+sudo rpm -i splunk-8.2.2.1-ae6821b7c64b-linux-2.6-x86_64.rpm;
+sudo cd /opt/splunk/bin;
+sudo ./splunk start --accept-license;
+sudo ./splunk stop;
+sudo ./splunk enable boot-start -user splunk;
+sudo echo "$USER";
+sudo chown -R splunk:splunk /opt/splunk;
 sudo su - splunk;
-chmod 755 Splunk_Ent_Rpm.sh
-echo "$USER";
-/opt/splunk/bin/splunk start;
+sudo echo "$USER";
+sudo /opt/splunk/bin/splunk start;
